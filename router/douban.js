8a3db8841abe331ee61724e50fb9fea2f9040fdb
Derycktse <285205451@qq.com>
2018-04-29 01:24:48 +0800

    两页一次

--- router/douban.js
+++ router/douban.js
@@ -11,33 +11,42 @@ const instance = axios.create({
   timeout: 60000,
   headers: {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-    'Referer' : 'https://www.douban.com/group/shenzhen/',
-    'Connection' : 'keep-alive',
-    'Pragma' : 'no-cache',
-    'Cache-Control' : 'no-cache',
-    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding' : 'gzip, deflate, br',
-    'Accept-Language' : 'en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6',
-    'Cookie' : 'll="118282"; bid=7z57_X3aaME; _pk_ses.100001.8cb4=*; __utma=30149280.884327805.1524932197.1524932197.1524932197.1; __utmc=30149280; __utmz=30149280.1524932197.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); as="https://sec.douban.com/b?r=https%3A%2F%2Fwww.douban.com%2Fgroup%2Fshenzhen%2F"; ps=y; ck=2NOd; __utmt=1; push_noty_num=0; push_doumail_num=0; ap=1; _ga=GA1.2.884327805.1524932197; _gid=GA1.2.376286299.1524933837; __yadk_uid=Z5Bk9MmBmlmrE795aeWbRVC4UmRMHMzr; _pk_id.100001.8cb4=2fa688e9bded6cd0.1524932196.1.1524933864.1524932196.; __utmb=30149280.37.4.1524933864107' 
+    'Referer': 'https://www.douban.com/group/shenzhen/',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh-TW;q=0.7,zh;q=0.6',
+    'Cookie': 'll="118282"; bid=7z57_X3aaME; _pk_ses.100001.8cb4=*; __utma=30149280.884327805.1524932197.1524932197.1524932197.1; __utmc=30149280; __utmz=30149280.1524932197.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); as="https://sec.douban.com/b?r=https%3A%2F%2Fwww.douban.com%2Fgroup%2Fshenzhen%2F"; ps=y; ck=2NOd; __utmt=1; push_noty_num=0; push_doumail_num=0; ap=1; _ga=GA1.2.884327805.1524932197; _gid=GA1.2.376286299.1524933837; __yadk_uid=Z5Bk9MmBmlmrE795aeWbRVC4UmRMHMzr; _pk_id.100001.8cb4=2fa688e9bded6cd0.1524932196.1.1524933864.1524932196.; __utmb=30149280.37.4.1524933864107'
   }
 });
 
 
 router.get('/douban/group/:groupname', async (ctx, next) => {
   let rawResultList = []
-  // const res = await axios.get(`${DOUBAN_URL}group/${ctx.params.groupname}/discussion?start=0`)
 
-  const res = await instance.get(`/group/${ctx.params.groupname}/discussion?start=0`, {
-
+  // 获取第一页列表
+  const resPage1 = await instance.get(`/group/${ctx.params.groupname}/discussion?start=0`, {
   })
-  const $ = cheerio.load(res.data)
-
+  const $ = cheerio.load(resPage1.data)
 
   // 获取列表里的文章链接
-  const targetUrlList = $('#content td.title a').map((idx, item) => {
+  let targetUrlList = $('#content td.title a').map((idx, item) => {
     return item.attribs.href;
   })
 
+  const resPage2 = await instance.get(`/group/${ctx.params.groupname}/discussion?start=25`, {
+  })
+  const $ = cheerio.load(resPage2.data)
+
+  // 获取列表里的文章链接
+  let targetUrlList2 = $('#content td.title a').map((idx, item) => {
+    return item.attribs.href;
+  })
+
+  // 合并两页
+  targetUrlList.push(...targetUrlList2)
 
   // 获取小组的标题
   const urlTitle = $('title').text().trim();
@@ -47,7 +56,7 @@ router.get('/douban/group/:groupname', async (ctx, next) => {
   feedOptions.feedUrl = `${DOUBAN_URL}feed/group/${ctx.params.groupname}/discussion`;
 
   // 获取文章结果
-  await fetchUrl(targetUrlList, 3, rawResultList)
+  await fetchUrl(targetUrlList, 5, rawResultList)
 
 
   // 处理内容
