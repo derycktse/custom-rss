866c5209c5ff37900eb39a098eccf783403ea074
derycktse <285205451@qq.com>
2018-04-28 14:37:45 +0800

    调整

--- router/douban.js
+++ router/douban.js
@@ -7,21 +7,30 @@ const router = new Router()
 const DOUBAN_URL = 'https://www.douban.com/'
 
 
-router.get('/douban', async (ctx, next) => {
+router.get('/douban/group/:groupname', async (ctx, next) => {
   let rawResultList = []
-  const res = await axios.get(`${DOUBAN_URL}group/shenzhen/discussion?start=0`)
-  console.log(res.data)
+  const res = await axios.get(`${DOUBAN_URL}group/${ctx.params.groupname}/discussion?start=0`)
   const $ = cheerio.load(res.data)
+
+
+  // 获取列表里的文章链接
   const targetUrlList = $('#content td.title a').map((idx, item) => {
-    // console.log(item.href)
     return item.attribs.href;
   })
 
 
+  // 获取小组的标题
+  const urlTitle = $('title').text().trim();
+
+  const feedOptions = {}
+  feedOptions.title = urlTitle;
+  feedOptions.feedUrl = `${DOUBAN_URL}feed/group/${ctx.params.groupname}/discussion`;
+
+  // 获取文章结果
   await fetchUrl(targetUrlList, 3, rawResultList)
 
-  console.log(rawResultList[0])
 
+  // 处理内容
   contentList = rawResultList.map(stripContent)
   console.log(contentList)
 
@@ -34,6 +43,11 @@ router.get('/douban', async (ctx, next) => {
 })
 
 
+
+/**
+ * 根据URL去获取文章的内容，
+ * 支持写入同时并发的url数量
+ */
 async function fetchUrl(list, count, rawResultList) {
   const subList = list.splice(0, count);
   if (!subList.length) return
@@ -44,12 +58,10 @@ async function fetchUrl(list, count, rawResultList) {
 
   const resList = await Promise.all(pmArr);
 
-  // rawResultList = rawResultList.concat(resList.map((res) => { return res.data }))
   rawResultList.push(...resList.map((res) => {
     return {
-      // url: 
       html: res.data,
-      url : `${DOUBAN_URL}${res.request.path}`
+      url: `${DOUBAN_URL}${res.request.path}`
     }
   }))
 
@@ -61,15 +73,19 @@ async function fetchUrl(list, count, rawResultList) {
 
 }
 
-function stripContent(html) {
-  let $ = cheerio.load(html)
+
+/**
+ * 将需要的内容从获取的html 字符串中提取出来
+ * @param  {} html
+ */
+function stripContent(rawItem) {
+  let $ = cheerio.load(rawItem.html)
   let content = ''
   const item = {}
   try {
-
+    item.url = rawItem.url
     item["title"] = $('h1').text().trim()
     content = $('#link-report').html().replace(/<\/?[^>]+(>|$)/g, "")
-    // content = html.replace(/<\/?[^>]+(>|$)/g, "")
   }
   catch (e) {
     console.log('strip error')
@@ -78,14 +94,13 @@ function stripContent(html) {
 }
 
 
-function rssFactory(option) {
-  const feed = new RSS(feedOptions);
+function rssFactory(feedOptions) {
   /* lets create an rss feed */
   const feed = new RSS({
-    title: 'title',
+    title: feedOptions.title,
     description: 'description',
-    feed_url: 'https://douban.com/rss.xml',
-    site_url: 'https://www.douban.com',
+    feed_url: feedOptions.feedUrl,
+    site_url: `${DOUBAN_URL}`,
     image_url: 'https://img3.doubanio.com/favicon.ico',
     managingEditor: 'derycktse',
     webMaster: 'derycktse',
@@ -99,7 +114,30 @@ function rssFactory(option) {
     },
   });
 
-  feed.item({})
+  feed.item({
+    title: 'item title',
+    description: 'use this for the content. It can include html.',
+    url: 'http://example.com/article4?this&that', // link to the item
+    // guid: '1123', // optional - defaults to url
+    categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4'], // optional - array of item categories
+    // author: 'Guest Author', // optional - defaults to feed author property
+    date: 'May 27, 2012', // any format that js Date can parse.
+    lat: 33.417974, //optional latitude field for GeoRSS
+    long: -111.933231, //optional longitude field for GeoRSS
+    enclosure: { url: '...', file: 'path-to-file' }, // optional enclosure
+    custom_elements: [
+      { 'itunes:author': 'John Doe' },
+      { 'itunes:subtitle': 'A short primer on table spices' },
+      {
+        'itunes:image': {
+          _attr: {
+            href: 'http://example.com/podcasts/everything/AllAboutEverything/Episode1.jpg'
+          }
+        }
+      },
+      { 'itunes:duration': '7:04' }
+    ]
+  });
 }
 
 
